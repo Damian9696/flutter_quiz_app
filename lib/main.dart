4a7a53feca160a6e12b6ed0a5e2683fbdaf2304d
Damian9696 <damian.ogorek.1996@gmail.com>
2021-05-27 21:15:05 +0200

    Updated questions to hold score on each answer; Updated result widget;

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
+
   final _questions = const [
     {
       "question": "Date of initial release Flutter?",
-      "answers": ["2015", "2016", "2017", "2018"]
+      "answers": [
+        {"text": "2015", "score": 0},
+        {"text": "2016", "score": 0},
+        {"text": "2017", "score": 1},
+        {"text": "2018", "score": 0}
+      ]
     },
     {
       "question": "Where is the local capital of Poland?",
-      "answers": ["Warsaw", "Cracow", "Gdynia", "Sopot"]
+      "answers": [
+        {"text": "Warsaw", "score": 1},
+        {"text": "Cracow", "score": 0},
+        {"text": "Gdynia", "score": 0},
+        {"text": "Sopot", "score": 0}
+      ]
     },
     {
       "question": "The founder of PayPal is...",
-      "answers": ["Donald Trump", "Bill Gates", "Elon Musk", "Jeff Bezos"]
+      "answers": [
+        {"text": "Donald Trump", "score": 0},
+        {"text": "Bill Gates", "score": 0},
+        {"text": "Elon Musk", "score": 1},
+        {"text": "Jeff Bezos", "score": 0}
+      ]
     },
     {
       "question": "The speed of light is...",
-      "answers": ["399693 m/s", "399793 m/s", "299693 m/s", "299793 m/s"]
+      "answers": [
+        {"text": "399693 m/s", "score": 0},
+        {"text": "399793 m/s", "score": 0},
+        {"text": "299693 m/s", "score": 0},
+        {"text": "299793 m/s", "score": 1}
+      ]
     },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex += 1;
     });
@@ -48,10 +72,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: _questions,
-                questionIndex: _questionIndex)
-            : Result(),
+            answerQuestion: _answerQuestion,
+            questions: _questions,
+            questionIndex: _questionIndex)
+            : Result(_totalScore),
       ),
     );
   }
